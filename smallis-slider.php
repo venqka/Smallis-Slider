a2a4bea98c43af36e80c1fc5bbe9de8be1337337
venelinb <venelinborisov5@gmail.com>
2019-12-12 17:18:39 +0200

    Create smallis slider

--- smallis-slider.php
+++ smallis-slider.php
@@ -20,7 +20,10 @@ function smallis_slider_enqueue() {
 		wp_enqueue_style( 'smallis-slider-styles', plugin_dir_url( __FILE__ ) . 'style.css', array(), '1.0', false );
 
 		//enqueue slick styles
-		wp_enqueue_style( 'slick-styles', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick.css', array(), '1.8.1', false );
+		wp_enqueue_style( 'slick-styles', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick.css', array(), '', false );
+
+		//enqueue slick styles
+		wp_enqueue_style( 'slick-theme-styles', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick-theme.css', array(), '', false );
 
 		//Enqueue slider scripts
 		wp_register_script( 'smallis-slider-scripts', plugin_dir_url( __FILE__ ) . 'scripts.js', array('jquery'), '1.0', false );
@@ -37,3 +40,54 @@ function smallis_slider_enqueue() {
 add_action( 'wp_enqueue_scripts', 'smallis_slider_enqueue' );
 
 include( 'slider-post-type.php' );
+
+function smallis_slider() {
+
+	$slides_args = array(
+		'post_type'			=>	'smallis-slider',
+		'posts_per_page'	=>	-1,
+	);
+	$slider = NEW WP_Query( $slides_args );
+
+	if( $slider->have_posts() ) {
+		ob_start();
+?>
+			<div class="smallis-container">
+				<div class="smallis-slider-container smallis-slider">
+<?php			
+					while( $slider->have_posts() ) {
+						$slider->the_post();
+?>
+						<div class="smallis-slide" style="background-image: url(<?php echo the_post_thumbnail_url(); ?>)">
+							<div class="smallis-slider-overlay">
+								<div class="smallis-slide-title">
+									<?php echo the_title( '<h3>', '</h3>' ); ?>
+								</div>
+								<div class="smallis-slide-content">
+									<?php echo the_content(); ?>
+								</div>
+								<div class="smallis-slide-button-container">
+<?php
+									$smallis_slide_button_url = get_post_meta( get_the_ID(), 'slide-url', true );
+									$smallis_slide_button_text = get_post_meta( get_the_ID(), 'button-text', true );
+
+									if( !empty( $smallis_slide_button_url ) ) {
+?>
+										<a href="<?php echo $smallis_slide_button_url; ?>" rel="noopener noreferer" target="blank"><span class="smallis-slide-button"><?php echo $smallis_slide_button_text; ?></span></a>
+<?php										
+									}
+?>
+								</div>	
+							</div>	
+						</div>	
+<?php
+					}	
+?>
+				</div><!--smallis-slider-container -->	
+			</div>	
+<?php
+		$smallis_slider = ob_get_clean();
+		echo $smallis_slider;
+	}	
+}
+add_action( 'smallis_home_before_content', 'smallis_slider' );
