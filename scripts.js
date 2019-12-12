4740bcde6628870e0205085305ba1f8bffc08f5e
venelinb <venelinborisov5@gmail.com>
2019-12-12 17:18:55 +0200

    Initialize slick slider

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function(){
-	
+	jQuery('.smallis-slider').slick({
+  		dots: true,
+  		infinite: true,
+  		speed: 300,
+  		slidesToShow: 1,
+  		// centerMode: true,
+  		// variableWidth: true
+	});    
 });
