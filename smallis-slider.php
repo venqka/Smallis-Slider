c4884f927495bc6a085535fe0909a236f43a0a60
venelinb <venelinborisov5@gmail.com>
2019-12-12 12:33:35 +0200

    Initial commit

---
+++ smallis-slider.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Smallis Slider
+ * Plugin URI:        https://github.com/venqka/Smallis-Slider
+ * Description:       This plugin adds a slider to the Smallis Front page.
+ * Version:           1.0.0
+ * Author:            venqka
+ * Author URI:        vensio.eu
+ * Plugin Type: Piklist
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       smallis
+ * Domain Path:       /languages
+ */
