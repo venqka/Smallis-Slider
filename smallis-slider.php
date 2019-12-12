763925f1a759e527acbebd6c5245bf3bc7451546
venelinb <venelinborisov5@gmail.com>
2019-12-12 13:09:13 +0200

    Add slick slider library

--- smallis-slider.php
+++ smallis-slider.php
@@ -17,12 +17,20 @@ function smallis_slider_enqueue() {
 
 	if( is_front_page() ) {
 		//enqueue slider styles
-		wp_enqueue_style( 'smallis-slider-styles', plugin_dir_url( __FILE__ ) . '/style.css', array(), '1.0', false );
+		wp_enqueue_style( 'smallis-slider-styles', plugin_dir_url( __FILE__ ) . 'style.css', array(), '1.0', false );
+
+		//enqueue slick styles
+		wp_enqueue_style( 'slick-styles', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick.css', array(), '1.8.1', false );
 
 		//Enqueue slider scripts
-		wp_register_script( 'smallis-slider-scripts', plugin_dir_url( __FILE__ ) . '/scripts.js', array('jquery'), '1.0', false );
+		wp_register_script( 'smallis-slider-scripts', plugin_dir_url( __FILE__ ) . 'scripts.js', array('jquery'), '1.0', false );
 
-		wp_enqueue_script( 'smallis-slider-scripts', plugin_dir_url( __FILE__ ) . '/scripts.js', array('jquery'), '1.0', false );
+		wp_enqueue_script( 'smallis-slider-scripts', plugin_dir_url( __FILE__ ) . 'scripts.js', array('jquery'), '1.0', false );
+
+		//Enqueue slick scripts
+		wp_register_script( 'slick-scripts', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick.js', array('jquery'), '1.8.1', false );
+
+		wp_enqueue_script( 'slick-scripts', plugin_dir_url( __FILE__ ) . 'assets/lib/slick/slick.js', array('jquery'), '1.8.10', false );
 	}
 
 }
